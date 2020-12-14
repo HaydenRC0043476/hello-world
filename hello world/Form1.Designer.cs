c44320c733e3909b1e62faca32afad9d17b5b07f
Hayden Mottram <rc0043476@student.riversidecollege.ac.uk>
2020-12-14 14:59:24 +0000

    added hello button

--- hello world/Form1.Designer.cs
+++ hello world/Form1.Designer.cs
@@ -28,12 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
+            this.txtBxname = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(524, 185);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(195, 69);
             this.button1.TabIndex = 0;
@@ -41,22 +44,39 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
+            // txtBxname
+            // 
+            this.txtBxname.Location = new System.Drawing.Point(12, 87);
+            this.txtBxname.Name = "txtBxname";
+            this.txtBxname.Size = new System.Drawing.Size(195, 20);
+            this.txtBxname.TabIndex = 1;
+            this.txtBxname.TextChanged += new System.EventHandler(this.TxtBxname_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtBxname);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtBxname;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
     }
 }
 
--- hello world/Form1.cs
+++ hello world/Form1.cs
@@ -24,7 +24,12 @@ namespace hello_world
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello World");
+            MessageBox.Show("Hello "+ txtBxname.Text);
+        }
+
+        private void TxtBxname_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
